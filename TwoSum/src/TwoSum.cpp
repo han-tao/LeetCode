18d964554b976f62387086632d41a19bca825786
hantao <m15147611754@163.com>
2018-02-10 11:42:43 +0800

    该方法是假设在nums升序排列的结果下进行的

---
+++ TwoSum/src/TwoSum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>     // std::cout  
+#include <algorithm>    // std::sort  
+#include <vector>       // std::vector  
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	vector<int> res;
+	int remainder;
+	int index;
+	for ( index = 0; index < nums.size(); ++index)
+	{
+		remainder = target - nums[index];
+		if (nums[index] < remainder)
+		{
+			for (int iindex = index+1; iindex < nums.size(); iindex++)
+			{
+				if (remainder == nums[iindex])
+				{
+					res.push_back(index+1);
+					res.push_back(iindex+1);
+				}
+			}
+		}
+	}
+	return res;
+}
+int main(int argc, char** argv) {
+	std::vector<int> nums = { 2, 7, 11, 15 };
+	int target = 9;
+	std::vector<int> res = twoSum(nums, target);
+	for (vector<int>::iterator iter = res.begin(); iter != res.end(); ++iter)
+	{
+		std::cout << *iter << '\t';
+	}
+	std::cin.get();
+	return 1;
+}
